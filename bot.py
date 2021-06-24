081bf65f9f08447959bf4a3d8130153852d80cf6
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-06-24 16:49:56 +0100

    fix restart command

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import subprocess
 git_head = subprocess.check_output(["git", "rev-parse", "HEAD"], universal_newlines=True)
 
 def restart():
-    os.system(f"./restart.sh {os.getpid()}")
+    os.system(f"kill -9 {os.getpid()}")
 
 def update():
     subprocess.call(["git", "stash"])
