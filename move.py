95a8b41b2c33b2f69ae9d1203ca340f4f49a42b2
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-07-14 13:55:03 +0100

    move to_string and from_string

--- move.py
+++ move.py
@@ -5,3 +5,23 @@ class Move(Enum):
     L = 1
     D = 2
     R = 3
+
+def to_string(move):
+    if move == Move.U:
+        return "U"
+    elif move == Move.L:
+        return "L"
+    elif move == Move.D:
+        return "D"
+    elif move == Move.R:
+        return "R"
+
+def from_string(move):
+    if move == "U":
+        return Move.U
+    elif move == "L":
+        return Move.L
+    elif move == "D":
+        return Move.D
+    elif move == "R":
+        return Move.R
