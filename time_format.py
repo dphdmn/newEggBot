25dfc60edef871a358a23cf236307156f1a64fbe
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-07-16 14:43:44 +0100

    time formatter

---
+++ time_format.py
@@ -0,0 +1,8 @@
+def format(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    if h == 0:
+        if m == 0:
+            return f"{s} seconds"
+        return f"{m} minutes, {s} seconds"
+    return f"{h} hours, {m} minutes, {s} seconds"
