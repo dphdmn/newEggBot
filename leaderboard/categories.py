c79ca2bceb512155e609c66fc10297ae39511d1f
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-08-01 21:56:10 +0100

    add categories module which reads categories from txt file

---
+++ leaderboard/categories.py
@@ -0,0 +1,14 @@
+with open("categories.txt", "r") as f:
+    data = f.read().strip()
+    f.close()
+
+data = [line.split(",") for line in data.split("\n")]
+
+categories = []
+for row in data:
+    categories.append({
+        "width"     : row[0],
+        "height"    : row[1],
+        "solvetype" : row[2],
+        "avglen"    : row[3]
+    })
