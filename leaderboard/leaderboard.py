b9045f32412607c1f52842a49a704e90612b3b76
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-08-01 20:35:05 +0100

    new get_leaderboard function

---
+++ leaderboard/leaderboard.py
@@ -0,0 +1,39 @@
+import os
+import requests
+
+def get_leaderboard(width=-1, height=-1, solvetype="any", avglen=-1, user=""):
+    url = os.environ['slidysim']
+    r = requests.post(url, data = {
+        "width"       : width,
+        "height"      : height,
+        "solvetype"   : solvetype,
+        "displaytype" : "Standard",
+        "avglen"      : avglen,
+        "pbtype"      : "time",
+        "sortby"      : "time",
+        "controls"    : "km",
+        "user"        : user,
+        "solvedata"   : 0,
+        "version"     : "28.3"
+    })
+
+    data = [line.split(",") for line in r.text[19:].split("<br>")[:-1]]
+
+    leaderboard = []
+    for row in data:
+        leaderboard.append({
+            "width"       : row[0],
+            "height"      : row[1],
+            "solvetype"   : row[2],
+            "displaytype" : row[3],
+            "user"        : row[4],
+            "time"        : row[5],
+            "moves"       : row[6],
+            "tps"         : row[7],
+            "avglen"      : row[8],
+            "controls"    : row[9],
+            "pbtype"      : row[10],
+            "timestamp"   : row[12]
+        })
+
+    return leaderboard
