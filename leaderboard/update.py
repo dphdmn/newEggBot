02d90094859f8f2bb83f20023894b255a60f769d
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-08-03 05:00:47 +0100

    functions for updating the webpage by writing the leaderboard data into a javascript file

---
+++ leaderboard/update.py
@@ -0,0 +1,50 @@
+import base64
+import datetime as dt
+import pickle
+import zlib
+import leaderboard.leaderboard as lb
+from replit import db
+
+def store_results():
+    table = lb.results_table()
+
+    # pickle, compress, convert to base64 so it can be stored in the db
+    pickled_table = pickle.dumps(table)
+    compressed_table = zlib.compress(pickled_table, level=9)
+    base64_table = base64.b64encode(compressed_table).decode()
+
+    # store in the database
+    today = dt.datetime.now().strftime("%Y-%m-%d")
+    db[f"leaderboard/data/{today}"] = base64_table
+
+def update_webpage():
+    dates = [x[17:] for x in db.prefix("leaderboard/data/")]
+    dates.sort()
+
+    # write dates into an array
+    file = "var dates = ["
+    for date in dates:
+        file += "\"" + date + "\","
+
+    file = file[:-1] + "];\n"
+
+    # write table data into the js variable data
+    file += "var data = ["
+    for date in dates:
+        base64_table = db["leaderboard/data/" + date]
+        compressed_table = base64.b64decode(base64_table.encode())
+        pickled_table = zlib.decompress(compressed_table)
+        table = pickle.loads(pickled_table)
+        table_str = lb.format_results_table(table)
+        file += table_str + ","
+
+    file = file[:-1] + "];\n"
+
+    # write to data file
+    with open("data.js", "w") as f:
+        f.write(file)
+        f.close()
+
+def update():
+    store_results()
+    update_webpage()
