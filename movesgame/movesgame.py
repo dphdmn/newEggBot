ceb935838ab1e4e39b1095237a300b88de81d3fe
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-08-08 09:37:51 +0100

    remove status db key and use a variable instead

--- movesgame/movesgame.py
+++ movesgame/movesgame.py
@@ -7,6 +7,7 @@ class MovesGame:
         self.bot = bot
         self.channel = bot.get_channel(channel_id)
         self.db_path = f"{self.channel.guild.id}/movesgame/{self.channel.id}/"
+        self.running = False
 
         # time per round in seconds
         self.delay = 8
@@ -15,11 +16,7 @@ class MovesGame:
         self.block_size = 100
 
         # initialize db keys
-        db[self.db_path + "status"] = "closed"
         db[self.db_path + "lifetime_results"] = serialize.serialize({})
-
-    def is_open(self):
-        return db[self.db_path + "status"] == "open"
 
     def round_number(self):
         return db[self.db_path + "round_number"]
@@ -28,63 +25,63 @@ class MovesGame:
         key = self.db_path + "lifetime_results"
         return serialize.deserialize(db[key])
 
-    async def open(self):
-        if self.is_open():
-            pass
+    async def start(self):
+        if self.running:
+            return
+
+        self.running = True
+
+        # run the round
+        round = await MovesGameRound(self.bot, self.channel).run()
+
+        # store the history in blocks of n rounds per key
+        round_num = self.round_number()
+        block       = round_num // self.block_size
+        block_round = round_num  % self.block_size
+
+        # get the block and add the round to it
+        block_path = self.db_path + f"history/round_blocks/{block}"
+        if block_round == 0:
+            block_dict = {}
         else:
-            db[self.db_path + "status"] = "open"
+            block_dict = serialize.deserialize(db[block_path])
+        block_dict[block_round] = round
+        db[block_path] = serialize.serialize(block_dict)
 
-            # run the round
-            round = await MovesGameRound(self.bot, self.channel).run()
+        msg = "Good moves: " + ", ".join(round["good_moves"]) + "\n"
 
-            # store the history in blocks of n rounds per key
-            round_num = self.round_number()
-            block       = round_num // self.block_size
-            block_round = round_num  % self.block_size
+        # find winners and update lifetime results
+        lifetime_results = self.lifetime_results()
+        if len(round["results"]) == 0:
+            msg += "No one joined :("
+        else:
+            winners = []
+            for (id, m) in round["results"].items():
+                # if this is the users first movesgame, create lifetime results entries
+                if id not in lifetime_results:
+                    lifetime_results[id] = {
+                        "correct"   : 0,
+                        "incorrect" : 0
+                    }
 
-            # get the block and add the round to it
-            block_path = self.db_path + f"history/round_blocks/{block}"
-            if block_round == 0:
-                block_dict = {}
-            else:
-                block_dict = serialize.deserialize(db[block_path])
-            block_dict[block_round] = round
-            db[block_path] = serialize.serialize(block_dict)
-
-            msg = "Good moves: " + ", ".join(round["good_moves"]) + "\n"
-
-            # find winners and update lifetime results
-            lifetime_results = self.lifetime_results()
-            if len(round["results"]) == 0:
-                msg += "No one joined :("
-            else:
-                winners = []
-                for (id, m) in round["results"].items():
-                    # if this is the users first movesgame, create lifetime results entries
-                    if id not in lifetime_results:
-                        lifetime_results[id] = {
-                            "correct"   : 0,
-                            "incorrect" : 0
-                        }
-
-                    if m in round["good_moves"]:
-                        user = self.bot.get_user(id)
-                        winners.append(user.name)
-                        lifetime_results[id]["correct"] += 1
-                    else:
-                        lifetime_results[id]["incorrect"] += 1
-                
-                if len(winners) == 0:
-                    msg += "Everyone was wrong :egg:"
+                if m in round["good_moves"]:
+                    user = self.bot.get_user(id)
+                    winners.append(user.name)
+                    lifetime_results[id]["correct"] += 1
                 else:
-                    msg += "Winners: " + ", ".join(winners)
+                    lifetime_results[id]["incorrect"] += 1
+            
+            if len(winners) == 0:
+                msg += "Everyone was wrong :egg:"
+            else:
+                msg += "Winners: " + ", ".join(winners)
 
-            # store the updated lifetime results
-            db[self.db_path + "lifetime_results"] = serialize.serialize(lifetime_results)
+        # store the updated lifetime results
+        db[self.db_path + "lifetime_results"] = serialize.serialize(lifetime_results)
 
-            await self.channel.send(msg)
+        await self.channel.send(msg)
 
-            db[self.db_path + "status"] = "closed"
+        self.running = False
 
     def submit(self, user, move):
         db[self.db_path + f"current/results/{user.id}"] = move
