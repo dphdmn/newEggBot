523570ba5aed13c41f2a0d3370afde2fdb1cb5c5
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-02 20:07:11 +0100

    add name to each category

--- leaderboard/categories.py
+++ leaderboard/categories.py
@@ -6,9 +6,26 @@ data = [line.split(",") for line in data.split("\n")]
 
 categories = []
 for row in data:
+    width = int(row[0])
+    height = int(row[1])
+    solvetype = row[2]
+    avglen = int(row[3])
+
+    name = f"{width}x{height}"
+    if solvetype[:8] == "Marathon":
+        name += " x" + solvetype[9:]
+    elif solvetype == "2-N relay":
+        name += " relay"
+
+    if avglen == 1:
+        name += " single"
+    else:
+        name += f" ao{avglen}"
+
     categories.append({
-        "width"     : int(row[0]),
-        "height"    : int(row[1]),
-        "solvetype" : row[2],
-        "avglen"    : int(row[3])
+        "width"     : width,
+        "height"    : height,
+        "solvetype" : solvetype,
+        "avglen"    : avglen,
+        "name"      : name
     })
