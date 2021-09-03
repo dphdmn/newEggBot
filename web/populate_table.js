1e2469283515aa8b299fa8c76c54e4d01f2a4f0f
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-03 23:57:26 +0100

    fix show_results_from_date

--- web/populate_table.js
+++ web/populate_table.js
@@ -121,7 +121,7 @@ function populate_table(table){
 }
 
 function show_results_from_date(date){
-    const str = decompress(data[0]);
+    const str = decompress(data[date]);
     const table = JSON.parse(str);
     populate_table(table);
 }
