4848787f0ef4cb641a4b41ededc79714e010778a
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-12 08:14:54 +0100

    update flask app to send the webpage statically

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 from threading import Thread
-app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-@app.route('/')
+
+app = Flask(__name__, static_folder="./web")
+
+@app.route("/")
 def index():
-  return render_template("index.html")
-  
+    return send_from_directory("./web", "index.html")
+
+@app.route("/<path:path>")
+def other(path):
+    return send_from_directory("./web", path)
+
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
+
 def keep_alive():
     t = Thread(target=run)
     t.start()
