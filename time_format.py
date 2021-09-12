ee96a88869002aa9a002f1f79ba7235efe357aa3
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-12 10:21:24 +0100

    format function for formatting times like h:m:s.z

--- time_format.py
+++ time_format.py
@@ -6,3 +6,20 @@ def format_long(seconds):
             return f"{s} seconds"
         return f"{m} minutes, {s} seconds"
     return f"{h} hours, {m} minutes, {s} seconds"
+
+def format(time):
+    s, z = divmod(time, 1000)
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+
+    if time == -1:
+        return ""
+
+    if time < 1000:
+        return f"0.{z:03d}"
+    elif time < 60000:
+        return f"{s}.{z:03d}"
+    elif time < 3600000:
+        return f"{m}:{s:02d}.{z:03d}"
+    else:
+        return f"{h}:{m:02d}:{s:02d}.{z:03d}"
