a85f8737bacd9d4aebbb921ca5bb01f506edec4e
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-15 17:16:43 +0100

    return non-lower-case version of username in find_username

--- leaderboard/username.py
+++ leaderboard/username.py
@@ -3,19 +3,19 @@ from replit import db
 
 # find the first username which contains s as a substring
 def find_username(s):
-    s = s.lower()
-
     # get all usernames from the db, and convert to lower case
     names = serialize.deserialize(db["leaderboard/usernames"])
-    names = [name.lower() for name in names]
+    names_lower = [name.lower() for name in names]
 
     # check if there's an exact match
-    if s in names:
-        return s
+    if s.lower() in names_lower:
+        # return the non-lower-case version of the username
+        index = names_lower.index(s.lower())
+        return names[index]
 
     # if not, find the first username containing s as a substring
     for name in names:
-        if s in name:
+        if s.lower() in name.lower():
             return name
 
     # none found
