9ae4155c75c7300b087ba5b44a3275c281da0ca9
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-15 16:22:23 +0100

    remove get_usernames function and just read them from the database

--- leaderboard/username.py
+++ leaderboard/username.py
@@ -1,20 +1,22 @@
 from helper import serialize
 from replit import db
 
-def get_usernames():
-    # get the most recent data that we have
-    date = db.prefix("leaderboard/data/")[-1]
-    data = db[date]
-
-    # decompress it and get the usernames
-    data = serialize.deserialize(data)
-    usernames = list(data.keys())
-
-    return usernames
-
 # find the first username which contains s as a substring
 def find_username(s):
-    for name in get_usernames():
-        if s.lower() in name.lower():
+    s = s.lower()
+
+    # get all usernames from the db, and convert to lower case
+    names = serialize.deserialize(db["leaderboard/usernames"])
+    names = [name.lower() for name in names]
+
+    # check if there's an exact match
+    if s in names:
+        return s
+
+    # if not, find the first username containing s as a substring
+    for name in names:
+        if s in name:
             return name
+
+    # none found
     raise ValueError("username not found")
