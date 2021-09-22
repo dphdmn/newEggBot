7e346c7ff01c19c149770b71fb5e69c9cc3ddbbd
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-23 00:26:07 +0100

    logging in solver

--- solver.py
+++ solver.py
@@ -1,3 +1,4 @@
+from log import log
 import subprocess
 from algorithm import Algorithm
 
@@ -6,15 +7,20 @@ class Solver:
         self.size = size
 
     def start(self):
-        self.process = subprocess.Popen(f"./solver{self.size}", stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        program = f"./solver{self.size}"
+        log.info(f"starting solver \"{program}\"")
+        self.process = subprocess.Popen(program, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 
     def stop(self):
+        log.info(f"terminating solver {self.size}")
         self.process.terminate()
 
     def solve(self, scramble):
         # check that the scramble is solvable
         if not scramble.solvable():
             raise ValueError(f"puzzle state \"{scramble}\" is not solvable")
+
+        log.info(f"solving scramble: {scramble}")
 
         self.process.stdin.write(f"{scramble}\n")
         self.process.stdin.flush()
@@ -25,19 +31,25 @@ class Solver:
             if line == "done":
                 break
             else:
-                solutions.append(Algorithm(line))
+                solution = Algorithm(line)
+                solutions.append(solution)
+                log.info(f"found solution: {solution}")
 
+        log.info(f"solver finished, found {len(solutions)} solutions")
         return solutions
 
     def solveOne(self, scramble):
+        log.info("set solver to mode \"one\"")
         self.process.stdin.write("one\n")
         return self.solve(scramble)[0]
 
     def solveGood(self, scramble):
+        log.info("set solver to mode \"good\"")
         self.process.stdin.write("good\n")
         return self.solve(scramble)
 
     def solveAll(self, scramble):
+        log.info("set solver to mode \"all\"")
         self.process.stdin.write("all\n")
         return self.solve(scramble)
 
