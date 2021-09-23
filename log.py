3af1e98e1082cad20555c3c0fd53ff7f8f466406
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-23 23:41:23 +0100

    print less logging info to stdout

--- log.py
+++ log.py
@@ -3,11 +3,12 @@ import sys
 
 # set up logging
 log = logging.getLogger("egg_logger")
-formatter = logging.Formatter("[%(levelname)s][%(asctime)s][%(filename)s, %(funcName)s, %(lineno)d] %(message)s")
 file_handler = logging.FileHandler(filename="log.log")
-file_handler.setFormatter(formatter)
+file_formatter = logging.Formatter("[%(levelname)s][%(asctime)s][%(filename)s, %(funcName)s, %(lineno)d] %(message)s")
+file_handler.setFormatter(file_formatter)
 stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setFormatter(formatter)
+stdout_formatter = logging.Formatter("%(message)s")
+stdout_handler.setFormatter(stdout_formatter)
 log.addHandler(file_handler)
 log.addHandler(stdout_handler)
 log.setLevel(logging.DEBUG)
