07b1047a5a7b3ee57636accfe973d3e00ec7d2ed
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-24 19:27:24 +0100

    add egg_admin permission

--- permissions.py
+++ permissions.py
@@ -1,6 +1,17 @@
 import os
+from helper import serialize
+from replit import db
 
 owner_id = int(os.environ["owner"])
 
 def is_owner(user):
     return user.id == owner_id
+
+def is_egg_admin(user):
+    key = "permissions/egg_admin"
+
+    if key not in db:
+        db[key] = serialize.serialize({})
+
+    admins = serialize.deserialize(db[key])
+    return user.id in admins
