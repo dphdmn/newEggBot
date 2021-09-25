36fbe9b9a2e1cf60ff71afbe48a7d6f6d4363bb1
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-25 01:38:20 +0100

    raise exception instead of returning None

--- leaderboard/link.py
+++ leaderboard/link.py
@@ -26,11 +26,11 @@ def get_leaderboard_user(user_id):
     accounts = linked_accounts()
     if user_id in accounts:
         return accounts[user_id]
-    return None
+    raise ValueError("user has no linked account")
 
 def get_discord_user(lb_username):
     accounts = linked_accounts()
     for user_id, lb_user in accounts.items():
         if lb_user == lb_username:
             return user_id
-    return None
+    raise ValueError("user has no linked account")
