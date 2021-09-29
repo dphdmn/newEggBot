d55bd18ad88dc19cb0423659873747c033aaca6b
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-09-29 02:02:47 +0100

    place function

--- leaderboard/ranking.py
+++ leaderboard/ranking.py
@@ -12,3 +12,11 @@ def power(results_list):
 # sort rows of the table by power
 def sort_table(results_table):
     return dict(sorted(results_table.items(), key=lambda x: -power(x[1])))
+
+def place(results_table, user):
+    sorted_table = sort_table(results_table)
+    usernames = sorted_table.keys()
+    for i, name in enumerate(usernames):
+        if user == name:
+            return i+1
+    raise ValueError("username not found")
