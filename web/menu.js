4c3d18b3388c1e9e36d6e5bd94df4b15a26a8000
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2021-12-30 17:10:23 +0000

    try to improve formatting

--- web/menu.js
+++ web/menu.js
@@ -1,12 +1,12 @@
-import('https://kit.fontawesome.com/9fa2f75402.js') /* font awesome icons */
+import("https://kit.fontawesome.com/9fa2f75402.js") /* font awesome icons */
 
-let menuBtn = $('.menu-btn');
-let menu = $('.menu');
-let menuIcon = $('.menu-icon');
-let menuToggle = () =>{
-    menu.toggleClass('menu-active');
-    menuIcon.toggleClass('menu-icon-open');
-    $('body').toggleClass('overflow-hidden');
+let menuBtn = $(".menu-btn");
+let menu = $(".menu");
+let menuIcon = $(".menu-icon");
+let menuToggle = () => {
+    menu.toggleClass("menu-active");
+    menuIcon.toggleClass("menu-icon-open");
+    $("body").toggleClass("overflow-hidden");
 }
 
 // when clicking outside the menu to close it
@@ -15,12 +15,12 @@ $(document).click((e) => {
   	    menuToggle();
     }
 });
+
 menuBtn.click((e) => {
     e.stopPropagation();
     menuToggle();
 });
+
 menu.click((e) => {
     e.stopPropagation();
 });
-
-
--- web/switch.js
+++ web/switch.js
@@ -1,37 +1,38 @@
 let tierlist = ["gamma", "aleph", "ascended", "nova", "grandmaster", "master", "diamond", "platinum", "gold", "silver", "bronze", "beginner"];
 let switchBtn = $("#switch");
 
-switchBtn.on('change', function() {
+switchBtn.on("change", function(){
     tierlist.forEach((tier) => {
         changeTable(tier);
     });
 });
 
-function changeTable(tier_table) {
-  let elements = $(`#${tier_table}-table`).children($("tbody")).children(".player-row").children();
-  
-  $(elements).each((i) => {
-    let element = $(elements[i]);
-    let tier = element.attr("tier");
+function changeTable(tier_table){
+    let elements = $(`#${tier_table}-table`).children($("tbody")).children(".player-row").children();
 
-    if (tier) {
-        if (tierlist.indexOf(tier) <= tierlist.indexOf(tier_table)) {
-            if (tierlist.indexOf(tier) < tierlist.indexOf(tier_table)) {
-                if (switchBtn.is(':checked')) {
-                    $(element).css("font-weight", 800);
-                } else {
-                    $(element).css("font-weight", "");
+    $(elements).each((i) => {
+        let element = $(elements[i]);
+        let tier = element.attr("tier");
+
+        if(tier) {
+            if(tierlist.indexOf(tier) <= tierlist.indexOf(tier_table)){
+                if(tierlist.indexOf(tier) < tierlist.indexOf(tier_table)){
+                    if(switchBtn.is(":checked")){
+                        $(element).css("font-weight", 800);
+                    }
+                    else{
+                        $(element).css("font-weight", "");
+                    }
                 }
             }
-        } else {
-            if (switchBtn.is(':checked')) {
-                $(element).css("background-color", "grey");
-            } else {
-              $(element).css("background-color", "");
-
+            else{
+                if(switchBtn.is(":checked")){
+                    $(element).css("background-color", "grey");
+                }
+                else{
+                    $(element).css("background-color", "");
+                }
             }
         }
-    }
-  });
+    });
 }
-
