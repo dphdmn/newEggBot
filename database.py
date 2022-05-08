ee208d9967f14cd30b3ed3a2c01405ba9044983c
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-05-08 15:28:33 +0100

    keep a local copy of the database so we don't have to run a query to read anything

--- database.py
+++ database.py
@@ -4,28 +4,34 @@ from helper import serialize as s
 
 class Database:
     def __init__(self):
-        url = os.environ['DATABASE_URL']
-        self.conn = psycopg2.connect(url, sslmode='require')
+        url = os.environ["DATABASE_URL"]
+        self.conn = psycopg2.connect(url, sslmode="require")
+
+        # local copy so we don't need to send a query every time we just want to read something
+        self.local = self.query_all()
 
     def set(self, key, value, serialize=True):
         cur = self.conn.cursor()
+
         if serialize:
             v = s.serialize(value)
         else:
             v = value
+
+        self.local[key] = v
         cur.execute("""
             insert into egg (key, value) values (%s, %s)
             on conflict(key) do
             update set value=%s
         """, (key, v, v))
+
         self.conn.commit()
 
     def get(self, key, deserialize=True):
-        cur = self.conn.cursor()
-        cur.execute("select value from egg where key=%s order by key", (key,))
-        v = cur.fetchone()
-        if v is None:
+        if key not in self.local:
             raise KeyError(f"{key} not in database")
+
+        v = self.local[key]
         if deserialize:
             return s.deserialize(v[0])
         return v[0]
@@ -38,29 +44,31 @@ class Database:
 
     def __delitem__(self, key):
         cur = self.conn.cursor()
+
+        del self.local[key]
         cur.execute("delete from egg where key=%s", (key,))
 
     def __iter__(self):
         return self.keys()
 
     def __len__(self):
-        cur = self.conn.cursor()
-        cur.execute("select count(*) from egg")
-        a = cur.fetchone()
-        return a[0]
+        return len(self.local)
 
     def __contains__(self, key):
-        cur = self.conn.cursor()
-        cur.execute("select 1 from egg where key=%s", (key,))
-        a = cur.fetchone()
-        return a is not None
+        return key in self.local
 
     def prefix(self, pref):
-        cur = self.conn.cursor()
-        cur.execute("select key from egg where key like %s order by key", (pref + "%",))
-        return [x[0] for x in cur.fetchall()]
+        return sorted([k for k in self.local.keys() if k.startswith(pref)])
     
     def keys(self):
         return self.prefix("")
+
+    def query_all(self):
+        cur = self.conn.cursor()
+        cur.execute("select * from egg")
+        return dict(cur.fetchall())
+
+    def all(self):
+        return self.local
 
 db = Database()
