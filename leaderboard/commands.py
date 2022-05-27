dde7582fdd8cdd6a400f7fc3abede15ce8860e3e
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-05-27 22:45:44 +0100

    show all averages in getpb

--- leaderboard/commands.py
+++ leaderboard/commands.py
@@ -8,6 +8,7 @@ from leaderboard import commands_helper as helper
 import leaderboard.username as names
 from formatting import time as time_format
 from formatting import moves as moves_format
+import json
 
 def get_pb(width, height, user):
     username = names.find_username(user)
@@ -15,35 +16,65 @@ def get_pb(width, height, user):
     # get all the relevant data in one leaderboard call
     data = lb.get_leaderboard(width, height, user=username)
 
+    # {category: message text} pairs
+    results = {}
+
+    for result in data:
+        result_msg = ""
+        category = {
+            "width": width,
+            "height": height,
+            "solvetype": result["solvetype"],
+            "avglen": result["avglen"]
+        }
+
+        # ranked category result
+        if category in categories:
+            idx = categories.index(category)
+
+            # find the users pb for this category
+            best_time = helper.category_pb(category, data)
+
+            # find the tier of this result
+            tier = tiers.result_tier(idx, best_time)
+            tier_name = helper.get_tier_name(tier)
+
+            # find the next tier above the users tier so we can show the requirement
+            next_tier = helper.get_next_tier(tier)
+
+            requirement_msg = helper.get_requirement_message(next_tier, idx)
+
+            result_msg += f"{category_names[idx]}: {time_format.format(best_time)} ({tier_name})"
+            if requirement_msg is not None:
+                result_msg += f" ({requirement_msg})"
+        # not a main category, but a standard average
+        elif result["solvetype"] == "Standard":
+            avglen = result["avglen"]
+
+            best_time = helper.category_pb(category, data)
+
+            if avglen == 1:
+                name = "single"
+            else:
+                name = f"ao{avglen}"
+
+            result_msg += f"{width}x{height} {name}: {time_format.format(best_time)}"
+        else:
+            continue
+
+        # convert category to string because dict isn't hashable
+        cat_str = json.dumps(category)
+        results[cat_str] = result_msg
+
+    results_list = [(json.loads(x), y) for (x, y) in results.items()]
+    standard = sorted([x for x in results_list if x[0]["solvetype"] == "Standard"], key=lambda x: x[0]["avglen"])
+    nonstandard = [x for x in results_list if x[0]["solvetype"] != "Standard"]
+    results = standard + nonstandard
+
     msg = f"{width}x{height} PBs for {username}\n"
     msg += "```\n"
-
-    # Sizes used in tier ranking
-    if (width, height) in helper.get_used_sizes(categories):
-        for i, category in enumerate(categories):
-            if category["width"] == width and category["height"] == height:
-                # find the users pb for this category
-                best_time = helper.category_pb(category,data)
-
-                # find the tier of this result
-                tier = tiers.result_tier(i, best_time)
-                tier_name = helper.get_tier_name(tier)
-
-                # find the next tier above the users tier so we can show the requirement
-                next_tier = helper.get_next_tier(tier)
-
-                requirement_msg = helper.get_requirement_message(next_tier,i)
-
-                msg += f"{category_names[i]}: {time_format.format(best_time)} ({tier_name})"
-                if requirement_msg is not None:
-                    msg += f" ({requirement_msg})"
-                msg += "\n"
-    # Other sizes
-    else:
-        best_time = helper.general_pb(data)
-        msg += f"{width}x{height} time: {time_format.format(best_time)}\n"
-
-    msg += "```"
+    msg += "\n".join([x[1] for x in results])
+    msg += "\n```"
 
     return msg
 
